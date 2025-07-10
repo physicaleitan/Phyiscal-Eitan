2511b324cedcc3a4fabb6287bd388abbab7016b0
physicaleitan <physicaleitan@gmail.com>
2025-07-10 07:30:52 +0000

    sync

--- server/index.js
+++ server/index.js
@@ -35,8 +35,6 @@ app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 10000, // 10 שניות timeout
   })
   .then(() => console.log("✅ Connected to MongoDB"))
